42e3a7e731776187585cc166e6bf1da5be9fb796
u040437 <u040437@daimi.au.dk>
2008-08-05 14:42:04 +0000

    MusicPlayer

---
+++ Sound/MusicPlayer.cpp
@@ -0,0 +1,81 @@
+
+#include <Sound/MusicPlayer.h>
+
+namespace OpenEngine {
+namespace Sound {
+
+MusicPlayer::MusicPlayer(Camera* inicam, ISoundSystem* inisystem) {
+	cam = inicam;
+	system = inisystem;
+	current = 0;
+	nextid = -1;
+	first = true;
+}
+
+MusicPlayer::~MusicPlayer() {
+
+}
+
+ISound* MusicPlayer::GetBackGroundSound(int id) {
+	return backgroundlist.at(id);
+}
+
+void MusicPlayer::SetCurrentBackgroundSound(int id) {
+	current = id;
+}
+
+int MusicPlayer::AddBackGroundSound(string filename) {
+
+	ISoundResourcePtr backsoundres = ResourceManager<ISoundResource>::Create(filename);
+	ISound* backsound = system->CreateSound(backsoundres);
+	backgroundlist.push_back(backsound);
+	nextid++;
+	return nextid;
+
+}
+
+void MusicPlayer::RemoveBackGroundSound(int id) {
+
+	backgroundlist.erase(backgroundlist.begin()+id,backgroundlist.begin()+id+1);
+
+}
+
+void MusicPlayer::Initialize() {
+
+}
+
+void MusicPlayer::Deinitialize() {
+
+}
+
+void MusicPlayer::Process(const float deltaTime, const float percent) {
+
+	if (!backgroundlist.empty()) {
+
+		ISound* currentsound = backgroundlist.at(current);
+		
+		if (first) {
+
+			first = false;
+			currentsound->Play();
+
+		}
+		else if (currentsound->GetPlaybackState() == ISound::STOPPED) {
+	
+			current++;
+			if (current == backgroundlist.size())
+				current = 0;
+			currentsound = backgroundlist.at(current);
+			currentsound->Play();
+
+		}
+
+		Vector<3, float> pos = cam->GetPosition();
+		currentsound->SetPosition(pos);
+
+	}
+
+}
+
+}
+}
---
+++ Sound/MusicPlayer.h
@@ -0,0 +1,52 @@
+
+#ifndef _SOUND_MUSICPLAYER_H_
+#define _SOUND_MUSICPLAYER_H_
+
+#include <Core/IModule.h>
+#include <Display/Camera.h>
+#include <Sound/ISound.h>
+#include <Sound/ISoundSystem.h>
+#include <Resources/ISoundResource.h>
+#include <Resources/ResourceManager.h>
+#include <vector>
+
+namespace OpenEngine {
+namespace Sound {
+
+using OpenEngine::Core::IModule;
+using OpenEngine::Display::Camera;
+using namespace OpenEngine::Scene;
+using namespace std;
+
+class MusicPlayer : public IModule {
+
+private:
+	int current, nextid;
+	bool first;
+	Camera* cam;
+	ISoundSystem* system;
+	vector<ISound*> backgroundlist;
+
+public:
+	MusicPlayer(Camera* inicam, ISoundSystem* system);
+	~MusicPlayer();
+
+	int AddBackGroundSound(string filename);
+	void RemoveBackGroundSound(int id);
+	ISound* GetBackGroundSound(int id);
+
+	void SetCurrentBackgroundSound(int id);
+
+	void Initialize();
+    void Process(const float deltaTime, const float percent);
+    void Deinitialize();
+	
+	bool IsTypeOf(const std::type_info& inf) { 
+        return typeid(MusicPlayer) == inf; 
+    }
+};
+
+} // NS Sound
+} // NS OpenEngine
+
+#endif
